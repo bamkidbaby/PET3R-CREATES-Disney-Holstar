ada80ac14a4df72eb92d66ba4a4d13e044a54a13
bamkidbaby <126984532+bamkidbaby@users.noreply.github.com>
2025-12-18 21:14:33 +0100

    Update slider-data.js

--- slider-data.js
+++ slider-data.js
@@ -1,45 +1,43 @@
 let movies = [
     {
         name: 'Star Wars',
-        des: "George Lucas's epic space opera franchise that began in 1977, following the battle between the heroic Rebel Alliance and the tyrannical Galactic Empire, featuring Jedi Knights, the mystical Force, and iconic characters like Luke Skywalker, Darth Vader, and Princess Leia.",
-        image: 'images/slide1.jpg'
+        des: "The original epic space saga about rebels fighting a tyrannical empire, featuring Jedi, the Force, and iconic characters.",
+        image: './images/slide1.jpg'
     },
 
     {
         name: 'Spider-Man: Miles Morales',
-        des: 'This story follows teenager Miles Morales as he develops his own spider-powers and learns to become Spider-Man while Peter Parker is away, balancing his new responsibilities with family expectations in his Brooklyn neighborhood.',
-        image: 'images/slide2.jpg'
+        des: 'A teenager develops spider-powers and learns to become Spider-Man while balancing family life in Brooklyn.',
+        image: './images/slide2.jpg'
     },
 
     {
         name: 'Zootopia 2',
-        des: "The upcoming sequel to Disney's 2016 hit will reunite rabbit police officer Judy Hopps and fox con artist-turned-cop Nick Wilde for new adventures in the mammal metropolis of Zootopia, though specific plot details are still under wraps.",
-        image: 'images/slide3.jpg'
+        des: "Sequel reuniting Judy Hopps and Nick Wilde for new adventures in the mammal metropolis.",
+        image: './images/slide3.jpg'
     },
 
     {
         name: 'Cars',
-        des: " Pixar's 2006 animated film about Lightning McQueen, a hotshot race car who gets stranded in the forgotten town of Radiator Springs on Route 66, where he learns valuable lessons about friendship, community, and what truly matters in life.",
+        des: "A hotshot race car learns about friendship and community after getting stranded in a small town.",
         image: './images/slide4.jpg'
     },
 
     {
         name: 'Avatar: The Way of Water',
-        des: "James Cameron's 2022 sequel follows Jake Sully and Neytiri as they raise their family on Pandora. When an ancient threat returns, they seek refuge with the oceanic Metkayina clan, exploring the stunning underwater world of Pandora while protecting their loved ones from the Sky People's return.",
-        image: 'images/slide5.jpg'
+        des: "Jake Sully and Neytiri protect their family by joining an oceanic clan on Pandora.",
+        image: './images/slide5.jpg'
     },
 
-        {
+    {
         name: 'Deadpool',
-        des: 'Ryan Reynolds stars as Wade Wilson, a former special forces operative turned mercenary who gains accelerated healing powers after a rogue experiment. As the wisecracking, fourth-wall-breaking antihero Deadpool, he seeks revenge on the man who nearly destroyed his life.',
-        image: 'images/slide6.jpg'
+        des: 'A wisecracking mercenary with healing powers seeks revenge as an antihero who breaks the fourth wall.',
+        image: './images/slide6.jpg'
     },
 
-        {
+    {
         name: 'Iron Man',
-        des: "Marvel's iconic armored Avenger, Tony Stark, who uses his genius intellect and advanced technology to fight evil in a powerful suit of armor equipped with repulsor rays, flight capabilities, and an array of weapons systems.",
-        image: 'images/slide7.jpg'
+        des: "Genius inventor Tony Stark fights evil using his advanced armored suit.",
+        image: './images/slide7.jpg'
     }
-
-
 ]
